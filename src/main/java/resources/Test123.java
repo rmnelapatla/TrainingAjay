17a13d16fae18bed18cc0bd3f9d23f2215da7bc7
ramana <rmnelapatla@users.noreply.github.com>
2024-04-12 09:23:06 +0530

    Update Test123.java
    delete line  in new code

--- src/main/java/resources/Test123.java
+++ src/main/java/resources/Test123.java
@@ -7,9 +7,7 @@ public class Test123 {
         // ramana 
         System.out.println("this is new string");
 
-        // day2
-        int age=29;
-        System.out.println(age);
+       
     
     }
 }
